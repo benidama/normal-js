898ecce3427891e6ae7c0c8b41c8d565d7362aa2
benidama <benidama02@gmail.com>
2024-12-01 18:25:21 +0000

    date addded

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ calculateCircumference(5);
 
 function timer() {
   const dates = new Date();
+  const my = new Date(24, 12, 1);
   document.getElementById("tim").innerHTML = dates.toDateString();
 }
 timer();
